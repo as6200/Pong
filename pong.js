2d640fa85d334c3e4b4c73270441bf8bc668d157
as6200 <aayush6200@outlook.com>
2021-09-05 14:37:44 -0500

    fix 3

--- pong.js
+++ pong.js
@@ -1,27 +1,22 @@
 var canvas = document.getElementById("canvas");
-const ctx = c.getContext("2d");
+const ctx = canvas.getContext("2d");
 const WIDTH = 600;
 const HEIGHT = 400;
 //ctx.moveTo(0,0);
 //ctx.lineTo(600,400);
 //ctx.stroke();
 
-var main = function() {
-    var gameLoop = function() {
-        update();
+function main () {
+    function gameLoop () {
         draw();
-
+        
         window.requestAnimationFrame(gameLoop, canvas);
     }
 
     window.requestAnimationFrame(gameLoop, canvas);
 }
 
-var update = function() {
-
-}
-
-var draw = function() {
+function draw () {
     ctx.fillstyle = '#A52A2A';
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 }
