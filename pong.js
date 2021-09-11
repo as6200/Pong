eac82d9d91b03d2a2299a812eb0a5cc309761631
as6200 <aayush6200@outlook.com>
2021-09-10 23:15:31 -0500

    Ball movement

--- pong.js
+++ pong.js
@@ -3,9 +3,10 @@ const ctx = canvas.getContext("2d"); // Gets context
 // Sets Width and Height
 const WIDTH = 600;
 const HEIGHT = 400;
-var scores = [0, 0]; // Player 1 and 2's score
+const pi = Math.PI;
+var scores = [0, 0]; // player1 1 and 2's score
 
-// Constructor for player
+// Constructor for player1
 class Player {
     constructor (x, y, width, height, color, speed, keyUp, keyDown) {
         this.x = x;
@@ -18,13 +19,14 @@ class Player {
         this.keyUp = keyUp;
         this.keyDown = keyDown;
 
-        // Draws player
+        // Draws player1
         this.draw = function () {
+        	ctx.strokeRect(this.x, this.y, this.width, this.height);
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         };
 
-        // Updates position of player
+        // Updates position of player1
         this.update = function () {
             for (let i = 0; i < keyStates.length; i++) {
                 if (keyStates[i] === this.keyUp) {
@@ -46,38 +48,94 @@ class Player {
 
 // Ball object
 ball = {
-    x: WIDTH/2,
-    y: HEIGHT/2,
-    vx: 5,
-    vy: 0,
-    direction: -1,
-    radius: 5,
-    color: 'white',
-    speed: 5,
+	x:   null,
+	y:   null,
+	vel: null,
 
-    //Draws circular ball
-    draw: function () {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        ctx.closePath();
-    },
+	side:  10,
+	speed: 8,
 
-    // Updates position of ball
-    update: function () {
-        
-    }
-}
+	/**
+	 * Serves the ball towards the specified side
+	 * 
+	 * @param  {number} side 1 right
+	 *                       -1 left
+	 */
+	serve: function(side) {
+		// set the x and y position
+		var r = Math.random();
+		this.x = side===1 ? player1.x+player1.width : player2.x - this.side;
+		this.y = (HEIGHT - this.side)*r;
+		// calculate out-angle, higher/lower on the y-axis =>
+		// steeper angle
+		var phi = 0.1*pi*(1 - 2*r);
+		// set velocity direction and magnitude
+		this.vel = {
+			x: side*this.speed*Math.cos(phi),
+			y: this.speed*Math.sin(phi)
+		}
+	},
 
-// Main function
+	/**
+	 * Update the ball position and keep it within the canvas
+	 */
+	update: function() {
+		// update position with current velocity
+		this.x += this.vel.x;
+		this.y += this.vel.y;
+		// check if out of the canvas in the y direction
+		if (0 > this.y || this.y+this.side > HEIGHT) {
+			// calculate and add the right offset, i.e. how far
+			// inside of the canvas the ball is
+			var offset = this.vel.y < 0 ? 0 - this.y : HEIGHT - (this.y+this.side);
+			this.y += 2*offset;
+			// mirror the y velocity
+			this.vel.y *= -1;
+		}
+		// helper function to check intesectiont between two
+		// axis aligned bounding boxex (AABB)
+		var AABBIntersect = function(ax, ay, aw, ah, bx, by, bw, bh) {
+			return ax < bx+bw && ay < by+bh && bx < ax+aw && by < ay+ah;
+		};
+
+		// check agplayer2nts target paddle to check collision in x
+		// direction
+		var pdle = this.vel.x < 0 ? player1 : player2;
+		if (AABBIntersect(pdle.x, pdle.y, pdle.width, pdle.height,
+				this.x, this.y, this.side, this.side)
+		) {	
+			// set the x position and calculate reflection angle
+			this.x = pdle===player1 ? player1.x+player1.width : player2.x - this.side;
+			var n = (this.y+this.side - pdle.y)/(pdle.height+this.side);
+			var phi = 0.25*pi*(2*n - 1); // pi/4 = 45
+			// calculate smash value and update velocity
+			var smash = Math.abs(phi) > 0.2*pi ? 1.5 : 1;
+			this.vel.x = smash*(pdle===player1 ? 1 : -1)*this.speed*Math.cos(phi);
+			this.vel.y = smash*this.speed*Math.sin(phi);
+		}
+
+		// reset the ball when ball outside of the canvas in the
+		// x direction
+		if (0 > this.x+this.side || this.x > WIDTH) {
+			this.serve(pdle===player1 ? 1 : -1);
+		}
+	},
+
+	/**
+	 * Draw the ball to the canvas
+	 */
+	draw: function() {
+        ctx.strokeRect(this.x, this.y, this.side, this.side);
+		ctx.fillRect(this.x, this.y, this.side, this.side);
+	}
+};
+
+// Mplayer2n function
 function main () {
 
-    // Creates 2 player instances
-    player1 = new Player(50, HEIGHT/2 + 25/2, 10, 50, 'white', 7, 'w', 's');
-    player2 = new Player(550, HEIGHT/2 + 25/2, 10, 50, 'white', 7, 'ArrowUp', 'ArrowDown');
+    // Creates 2 player1 instances
+    player1 = new Player(50, HEIGHT/2 - 25, 10, 50, 'white', 7, 'w', 's');
+    player2 = new Player(540, HEIGHT/2 - 25, 10, 50, 'white', 7, 'ArrowUp', 'ArrowDown');
 
     // Records current keys being pressed and stores in keyStates.
     keyStates = [];
@@ -91,8 +149,10 @@ function main () {
     document.addEventListener("keyup", function(evt) {
         delete keyStates[keyStates.indexOf(evt.key)];
     });
-
-    // Main loop
+    
+    
+		ball.serve(1);
+    // Mplayer2n loop
     function gameLoop () {
         update();
         draw();
@@ -107,7 +167,7 @@ function main () {
 function update() {
     player1.update();
     player2.update();
-    // ball.update();
+    ball.update();
 }
 
 // Draws to canvas
